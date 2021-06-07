ef99ff50103776a18f69b946b4cf8613cefc03bf
satyam <2019335@iiitdmj.ac.in>
2021-06-07 07:24:53 +0530

    Marker Added to the map

--- script.js
+++ script.js
@@ -19,17 +19,18 @@ if (navigator.geolocation) {
       //console.log(`https://www.google.co.in/maps/@${latitide}${longitude}`);
       const coords = [latitude, longitude];
 
-      var map = L.map('map').setView(coords, 13);
+      const map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng]).addTo(map).bindPopup('Workout').openPopup();
+      });
     },
     function () {
       alert('Position Not found');
